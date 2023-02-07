a5a4d617e109d6f7b801e4431caaad703d98b791
Dharshan Kumar A S <dharshankumar.as@zensar.com>
2023-02-07 16:30:10 +0530

    second movie app

--- app/[movie]/page.jsx
+++ app/[movie]/page.jsx
@@ -27,7 +27,7 @@ async function MovieId({ params }) {
   );
 
   const movieVideoData = await videoRes.json();
-  console.log("hi", movieVideoData);
+  console.log("hi", movieVideoData.results[0].key);
 
   return (
     <div className='m-4 md:mx-20 md:my-8'>
@@ -40,21 +40,15 @@ async function MovieId({ params }) {
         <h2 className='bg-green-600 inline-block my-2 py-2 px-4 rounded-md text-sm'>
           {movieData.status}
         </h2>
-        {movieVideoData &&
-          movieVideoData?.results?.map((video) => {
-            <>
-              <iframe
-                width='853'
-                height='480'
-                src={`https://www.youtube.com/embed/${video.key}`}
-                title='Why React.js is taking a new direction'
-                frameborder='0'
-                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-                allowfullscreen></iframe>
-              ;<h1>{video.name}</h1>
-            </>;
-          })}
-
+        <div className='w-[87vw] h-[60vh] my-4'>
+          <iframe
+            className='w-full h-full'
+            src={`https://www.youtube.com/embed/${movieVideoData?.results[0]?.key}`}
+            title='Why React.js is taking a new direction'
+            frameborder='0'
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+            allowfullscreen></iframe>
+        </div>
         <Image
           src={`https://image.tmdb.org/t/p/original${movieData.backdrop_path}`}
           className='my-12 w-full object-contain'
