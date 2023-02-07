bddfb6fe3d8f44f82e85063d5cc5a0c93fb544e6
Dharshan Kumar A S <dharshankumar.as@zensar.com>
2023-02-07 15:08:01 +0530

    second movie app

--- app/Movie.tsx
+++ app/Movie.tsx
@@ -19,12 +19,15 @@ function Movie({
       {posterPath && (
         <div className='border'>
           <Link href={`/${id}`}>
-            <Image
-              src={`https://image.tmdb.org/t/p/original${posterPath}`}
-              width={250}
-              height={350}
-              alt={title}
-            />
+            <div className='w-[250px] h-[350px]'>
+              <Image
+                src={`https://image.tmdb.org/t/p/original${posterPath}`}
+                width={250}
+                height={350}
+                className='w-full h-full'
+                alt={title}
+              />
+            </div>
           </Link>
         </div>
       )}
