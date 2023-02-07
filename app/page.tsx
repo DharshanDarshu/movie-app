4e6b69522dd66c90913bf89cfc9797831c9a6fc9
Dharshan Kumar A S <dharshankumar.as@zensar.com>
2023-02-07 15:52:02 +0530

    second movie app

--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,16 @@ async function HomePage() {
   );
 
   const trendingMovie = await trendingResponse.json();
+  const topRatedResponse = await fetch(
+    `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.TMDB_API_KEY}`,
+  );
+
+  const topRatedMovie = await topRatedResponse.json();
   return (
     <div className=''>
       <Banner movies={trendingMovie.results} />
       <h1 className='mx-8 text-xl font-bold mt-4'>
-        Top Rated
+        Trending
       </h1>
 
       <div className='flex gap-4 mx-8 my-4 hidden-over'>
@@ -32,6 +37,10 @@ async function HomePage() {
           />
         ))}
       </div>
+      <Movies
+        movies={topRatedMovie}
+        category='top rated'
+      />
       <Movies
         movies={popularMovie}
         category='popular'
