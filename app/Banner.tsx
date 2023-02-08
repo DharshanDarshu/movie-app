625e894932bb8cd3b113885c156e3b4174e1e7b3
Dharshan Kumar A S <dharshankumar.as@zensar.com>
2023-02-08 17:41:40 +0530

    creating movie app

--- app/Banner.tsx
+++ app/Banner.tsx
@@ -1,43 +1,27 @@
-"use client";
-
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
 
 type Props = {
   movies: any;
 };
 
 function Banner({ movies }: Props) {
-  const [movie, setMovie] = useState({
-    title: "",
-    backdrop_path: "",
-    poster_path: "",
-  });
-  useEffect(() => {
-    const random = Math.floor(
-      Math.random() * movies.length,
-    );
-    console.log(movies[random]);
-    setMovie(movies[random]);
-  }, []);
-
   return (
     <div>
       <Image
-        src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+        src={`https://image.tmdb.org/t/p/original${movies[12].backdrop_path}`}
         className='hidden md:inline-block w-full h-[80vh] object-cover'
         width={600}
         height={600}
         priority
-        alt={movie.title}
+        alt={movies[12].title}
       />
       <Image
-        src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+        src={`https://image.tmdb.org/t/p/original${movies[10].poster_path}`}
         className='inline-block md:hidden w-full h-[80vh] object-cover'
         width={600}
         height={600}
         priority
-        alt={movie.title}
+        alt={movies[12].title}
       />
     </div>
   );
