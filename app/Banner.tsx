c0eafb7f6bd20a2b9b92b23d8c9fb04b918a1407
Dharshan Kumar A S <dharshankumar.as@zensar.com>
2023-02-08 17:05:25 +0530

    creating movie app

--- app/Banner.tsx
+++ app/Banner.tsx
@@ -1,31 +1,43 @@
+"use client";
+
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 type Props = {
   movies: any;
 };
 
 function Banner({ movies }: Props) {
-  const random = Math.floor(Math.random() * movies.length);
-  console.log(movies[random]);
-  const movieRandom = movies[random];
+  const [movie, setMovie] = useState({
+    title: "",
+    backdrop_path: "",
+    poster_path: "",
+  });
+  useEffect(() => {
+    const random = Math.floor(
+      Math.random() * movies.length,
+    );
+    console.log(movies[random]);
+    setMovie(movies[random]);
+  }, []);
+
   return (
     <div>
       <Image
-        src={`https://image.tmdb.org/t/p/original${movieRandom.backdrop_path}`}
+        src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
         className='hidden md:inline-block w-full h-[80vh] object-cover'
         width={600}
         height={600}
         priority
-        alt={movieRandom.title}
+        alt={movie.title}
       />
       <Image
-        src={`https://image.tmdb.org/t/p/original${movieRandom.poster_path}`}
+        src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
         className='inline-block md:hidden w-full h-[80vh] object-cover'
         width={600}
         height={600}
         priority
-        alt={movieRandom.title}
+        alt={movie.title}
       />
     </div>
   );
